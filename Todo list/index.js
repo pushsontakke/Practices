b0ab5ca6693146384018ddf574e2fa090d727a82
pusontakke <pusontakke1@gmail.com>
2023-05-23 23:51:20 +0530

    Wireframe

--- Todo list/index.js
+++ Todo list/index.js
@@ -1,4 +1,30 @@
-function myfunc(){
-    let inp = document.getElementById("add-task__input");
-    console.log(inp.value);
+function myfunc() {
+  let inputTask = document.getElementById("add-task__input");
+  console.log(inputTask.value);
+  let addTask = document.getElementById("add-task");
+  // addTask.innerHTML = inputTask.value;
+  // addTask.style.display = "block";
+  // addTask.style.color = "white";
+  // addTask.style.backgroundColor = "green";
+  // addTask.style.padding = "10px";
+  // addTask.style.borderRadius = "5px";
+  // addTask.style.width = "200px";
+  // addTask.style.height = "50px";
+  // addTask.style.fontSize = "20px";
+  // addTask.style.margin = "10px";
+  // addTask.style.textAlign = "center";
+  // addTask.style.lineHeight = "50px";
+  // addTask.style.cursor = "pointer";
+  // addTask.style.transition = "all .5s";
+  // addTask.style.boxShadow = "0 0 10px green";
+  // addTask.style.border = "none";
+  // addTask.style.outline = "none";
+  // addTask.style.position = "absolute";
+  // addTask.style.top = "50%";
+  // addTask.style.left = "50%";
+  // addTask.style.transform = "translate(-50%, -50%)";
+  // addTask.style.zIndex = "100";
+  // addTask.style.transition = "all .5s";
+  // addTask.style.cursor = "pointer";
+
 }
